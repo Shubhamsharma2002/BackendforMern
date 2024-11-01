e738f4b4bcce24573669243911013931e1cc0f43
shubham sharma <shubhamtech004@gmail.com>
2024-11-01 19:00:41 +0530

    user registration model done

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -5,7 +5,7 @@ const register = async(req,res)=>{
    try {
         const d = req.body;
       //   console.log(req.body);
-         res.status(200).send(d)
+         res.status(201).send(d)
    } catch (error) {
       console.log(error);
    }
