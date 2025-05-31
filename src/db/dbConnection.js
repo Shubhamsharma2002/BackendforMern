5c64f3dbd8fb9d77d35b7e4a2558769597f487d5
shubham sharma <shubhamtech004@gmail.com>
2025-05-31 22:55:04 +0530

    some db confilict

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -2,19 +2,19 @@ import mongoose from "mongoose";
 
 
 const connectDB = async () =>{
-      try {
-        // printing the string
-    console.log(`${process.env.URI}`); 
+    //   try {
+    //     // printing the string
+    // console.log(`${process.env.URI}`); 
     
-       const connection =  await mongoose.connect(`${process.env.URI}/${process.env.DB_NAME}`)
-        console.log(`databse connect ::)  ${connection}`);
+    //    const connection =  await mongoose.connect(`${process.env.URI}/${process.env.DB_NAME}`)
+    //     console.log(`databse connect ::)  ${connection}`);
         
-      } catch (error) {
-        console.log("mongo db connection error");
-        console.log(error);
+    //   } catch (error) {
+    //     console.log("mongo db connection error");
+    //     console.log(error);
 
         
-      }
+    //   }
 }
 
 
